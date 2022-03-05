db729a5ca4fead7ac7951c344640dfc3f8f4b664
Colby <colbyescott333@gmail.com>
2022-03-05 12:10:56 -0800

    My Portfolio commit

---
+++ js.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const cdbounceleft = entry.target.querySelector('.bouncy');
+
+        if (entry.isIntersecting) {
+            cdbounceleft.classList.add('cd-bounce-left');
+            return;
+        }
+
+        cdbounceleft.classList.remove('cd-bounce-left');
+    });
+});    
+
+
+
+observer.observe(document.querySelector('.cd-intro'));
